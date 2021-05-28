d1955b9d0472b2925f5a7410755c991e93b78516
peng.chen <peng.chen@opay-inc.com>
2021-05-28 16:37:33 +0800

    20210528 gin router update

--- src/api/main.go
+++ src/api/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"accumulate/api/logic/control"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -9,27 +9,44 @@ import (
 func main() {
 	router := gin.Default()
 
-	// This handler will match /user/john but will not match /user/ or /user
-	router.GET("/user/:name", func(c *gin.Context) {
-		name := c.Param("name")
-		c.String(http.StatusOK, "Hello %s", name)
+	router.OPTIONS("/*action", func(c *gin.Context) {
+		c.String(http.StatusOK, "all")
 	})
 
-	// However, this one will match /user/john/ and also /user/john/send
-	// If no other routers match /user/john, it will redirect to /user/john/
-	router.GET("/user/:name/*action", func(c *gin.Context) {
-		name := c.Param("name")
-		action := c.Param("action")
-		message := name + " is " + action
-		c.String(http.StatusOK, message)
-	})
+	parametersInPathGroup := router.Group("/user")
+	{
+		// This handler will match /user/john but will not match /user/ or /user
+		parametersInPathGroup.GET("/:name", control.Rout.GetParametersAccurateInPath)
 
-	// For each matched request Context will hold the route definition
-	router.POST("/user/:name/*action", func(c *gin.Context) {
-		if c.FullPath() == "/user/:name/*action" {
-			fmt.Printf("true \n")
-		} // true
-	})
+		// However, this one will match /user/john/ and also /user/john/send
+		// If no other routers match /user/john, it will redirect to /user/john/
+		parametersInPathGroup.GET("/:name/*action", control.Rout.GetParametersAccurateInPath)
+
+		// For each matched request Context will hold the route definition
+		parametersInPathGroup.POST("/:name/*action", control.Rout.GetFullPath)
+	}
+
+	routerGroup := router.Group("")
+	{
+		// Query string parameters are parsed using the existing underlying request object.
+		// The request responds to a url matching:  /welcome?firstname=Jane&lastname=Doe
+		routerGroup.GET("/welcome", control.Rout.GetQueryString)
+
+		//POST /post?id=1234&page=1 HTTP/1.1
+		//Content-Type: application/x-www-form-urlencoded
+		//
+		//name=manu&message=this_is_great
+		routerGroup.POST("/post/form", control.Rout.PostForm)
+
+		//POST /post?ids[a]=1234&ids[b]=hello HTTP/1.1
+		//Content-Type: application/x-www-form-urlencoded
+		//
+		//names[first]=thinkerou&names[second]=tianou
+		router.POST("/post/map", control.Rout.PostMap)
+
+		router.POST("/upload/picture", control.Rout.UploadPicture)
+		router.POST("/upload/pictures", control.Rout.UploadPictures)
+	}
 
 	_ = router.Run(":10000")
 }
